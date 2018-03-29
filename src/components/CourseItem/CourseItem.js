9ea6f2c6471e7f3a3b798cee7212bfb9aa32162d
allenwhitedev <allenwhitedev@gmail.com>
2018-03-29 10:08:34 -0400

    Add CourseItem stylings to absolute position CourseItem in order to deal with dangerouslySetInnerHTML issues with large amounts of html from CKEditor

--- src/components/CourseItem/CourseItem.js
+++ src/components/CourseItem/CourseItem.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 
 const CourseItem = ({courseItem}) =>
 (
-	<div className='CourseItem' style={ {paddingLeft: '315px'} }>
+	<div className='CourseItem' style={ {paddingLeft: '315px', position: 'absolute', left: '0px', top: '100px'} }>
 		<h2>{ courseItem.title }</h2>
 		<hr />
 		<section style={ {padding: '15px'} } className='courseItemContent' dangerouslySetInnerHTML={ {__html: courseItem.content} }></section>
